e77049f706acd2fb8cdc34f9bef27d5bcd0d2631
Marek Bazylewicz <mbazyl2@gmail.com>
2017-06-16 00:16:22 +0200

    Added 'tank' element to the canvas. Modified CSS for centering canvas, and resized canvas element.

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 var canvas;
 var canvasContext;
 
+var tankBottom = 40;
+var tankHeight = 15;
+var tankGunBottom = 10;
+var tankGunHeight = 15;
+
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
-	//drawAll();
-	canvasContext.fillStyle = 'black';
-	canvasContext.fillRect(0,0, canvas.width, canvas.height);
+	drawAll();
 }
 
-/*
+
+
+// drawing all elements on canvas
 function drawAll() {
-	colorRect(0,0,canvas.width, canvas.height, 'black');
+	colorRectangle(0,0,canvas.width, canvas.height, 'black');
+	colorRectangle(canvas.width/2-tankBottom/2, 480, tankBottom, tankHeight, 'white');
+	colorRectangle(canvas.width/2-tankGunBottom/2, canvas.height-tankHeight-tankGunHeight, tankGunBottom, tankGunHeight, 'white');
 }
 
-// function which allows to use just one command in draw everything for creating each rectangle
-function colorRect(leftX, topY, width, height, drawColor) {
+
+
+
+// function which allows to use just one command in drawAll for creating each rectangle
+function colorRectangle(leftX, topY, width, height, drawColor) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.fillRect(leftX, topY, width, height)
 }
-*/
+// function for simplifying of ball drawing in drawAll function
+function colorCircle(){
+
+}
+
+
